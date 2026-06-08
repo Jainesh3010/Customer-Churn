4996372c5cfc9864e06712691855d26ea31dab94
Jainesh3010 <jaineshs301@gmail.com>
2026-06-08 18:44:24 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -7,72 +7,87 @@ model=joblib.load("churn_model.pkl")
 st.title("Customer Churn Prediction")
 
 gender=st.selectbox("Gender",["Male","Female"])
+
 seniorcitizen=st.selectbox("Senior Citizen",[0,1])
+
 partner=st.selectbox("Partner",["Yes","No"])
+
 dependents=st.selectbox("Dependents",["Yes","No"])
-tenure=st.slider("Tenure",0,72)
+
+tenure=st.number_input("Tenure",0,72)
 
 phoneservice=st.selectbox("Phone Service",["Yes","No"])
+
 multiplelines=st.selectbox("Multiple Lines",["Yes","No","No phone service"])
+
 internetservice=st.selectbox("Internet Service",["DSL","Fiber optic","No"])
 
 onlinesecurity=st.selectbox("Online Security",["Yes","No","No internet service"])
+
 onlinebackup=st.selectbox("Online Backup",["Yes","No","No internet service"])
+
 deviceprotection=st.selectbox("Device Protection",["Yes","No","No internet service"])
 
 techsupport=st.selectbox("Tech Support",["Yes","No","No internet service"])
+
 streamingtv=st.selectbox("Streaming TV",["Yes","No","No internet service"])
+
 streamingmovies=st.selectbox("Streaming Movies",["Yes","No","No internet service"])
 
 contract=st.selectbox("Contract",["Month-to-month","One year","Two year"])
 
 paperlessbilling=st.selectbox("Paperless Billing",["Yes","No"])
 
-paymentmethod=st.selectbox(
-    "Payment Method",["Electronic check","Mailed check","Bank transfer (automatic)","Credit card (automatic)"]
-)
+paymentmethod=st.selectbox("Payment Method",["Electronic check","Mailed check","Bank transfer (automatic)","Credit card (automatic)"])
 
-monthlycharges=st.number_input("Monthly Charges")
-totalcharges=st.number_input("Total Charges")
+monthlycharges=st.number_input("Monthly Charges",0.0,200.0)
 
-if st.button("Predict"):
+totalcharges=st.number_input("Total Charges",0.0,10000.0)
 
-    input_df=pd.DataFrame({
+input_df=pd.DataFrame({
 
-        "Gender":[gender],
-        "Seniorcitizen":[seniorcitizen],
-        "Partner":[partner],
-        "Dependents":[dependents],
-        "Tenure":[tenure],
-        "Phoneservice":[phoneservice],
-        "Multiplelines":[multiplelines],
-        "Internetservice":[internetservice],
-        "Onlinesecurity":[onlinesecurity],
-        "Onlinebackup":[onlinebackup],
-        "Deviceprotection":[deviceprotection],
-        "Techsupport":[techsupport],
-        "Streamingtv":[streamingtv],
-        "Streamingmovies":[streamingmovies],
-        "Contract":[contract],
-        "Paperlessbilling":[paperlessbilling],
-        "Paymentmethod":[paymentmethod],
-        "Monthlycharges":[monthlycharges],
-        "Totalcharges":[totalcharges]
+    "Gender":[gender],
+    "Seniorcitizen":[seniorcitizen],
+    "Partner":[partner],
+    "Dependents":[dependents],
+    "Tenure":[tenure],
+    "Phoneservice":[phoneservice],
+    "Multiplelines":[multiplelines],
+    "Internetservice":[internetservice],
+    "Onlinesecurity":[onlinesecurity],
+    "Onlinebackup":[onlinebackup],
+    "Deviceprotection":[deviceprotection],
+    "Techsupport":[techsupport],
+    "Streamingtv":[streamingtv],
+    "Streamingmovies":[streamingmovies],
+    "Contract":[contract],
+    "Paperlessbilling":[paperlessbilling],
+    "Paymentmethod":[paymentmethod],
+    "Monthlycharges":[monthlycharges],
+    "Totalcharges":[totalcharges]
 
-    })
+})
+
+input_df["Tenure"]=pd.to_numeric(input_df["Tenure"])
+
+input_df["Monthlycharges"]=pd.to_numeric(input_df["Monthlycharges"])
+
+input_df["Totalcharges"]=pd.to_numeric(input_df["Totalcharges"])
+
+if st.button("Predict Churn"):
 
     probability=model.predict_proba(input_df)[0][1]
 
     prediction=int(probability>0.2)
-    
+
     if prediction==1:
-    
+
         st.error("Customer likely to churn")
-    
+
         st.write(f"Churn Probability: {probability:.2%}")
-    
+
     else:
-    
+
         st.success("Customer likely to stay")
-    
+
         st.write(f"Stay Probability: {(1-probability):.2%}")
